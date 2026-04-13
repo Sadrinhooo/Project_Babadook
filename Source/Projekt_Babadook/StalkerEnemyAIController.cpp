3d678c1202e2a0b2ab29656126bdf0b3e260481c
Sadrinhooo <sadrabazi82@gmail.com>
2026-04-13 18:13:10 +0200

    BS AI Added

--- Source/Projekt_Babadook/StalkerEnemyAIController.cpp
+++ Source/Projekt_Babadook/StalkerEnemyAIController.cpp
@@ -2,4 +2,30 @@
 
 
 #include "StalkerEnemyAIController.h"
+#include "Kismet/GameplayStatics.h"
 
+void AStalkerEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	int numOFPLAYER = UGameplayStatics::GetNumPlayerControllers(GetWorld());
+	UE_LOG(LogTemp, Warning, TEXT("%d"), numOFPLAYER)
+	Player1 = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	
+}
+
+void AStalkerEnemyAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	if (LineOfSightTo(Player1))
+	{
+		SetFocus(Player1);
+		MoveToActor(Player1, 200);
+	}else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
+	
+}
--- Source/Projekt_Babadook/StalkerEnemyAIController.h
+++ Source/Projekt_Babadook/StalkerEnemyAIController.h
@@ -14,4 +14,14 @@ class PROJEKT_BABADOOK_API AStalkerEnemyAIController : public AEnemyAIController
 {
 	GENERATED_BODY()
 	
+public:
+	
+	APawn* Player1;
+	APawn* Player2;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	virtual void Tick(float DeltaTime) override;
 };
