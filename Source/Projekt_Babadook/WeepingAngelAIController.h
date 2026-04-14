0423a4bcebf7a6207e85abcde8bb52b6c19e2b9d
Gaz0lina <gazelle.rashid@outlook.com>
2026-04-14 16:57:24 +0200

    Weeping ur mom mechanism

---
+++ Source/Projekt_Babadook/WeepingAngelAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyAIController.h"
+#include "WeepingAngelAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJEKT_BABADOOK_API AWeepingAngelAIController : public AEnemyAIController
+{
+	GENERATED_BODY()
+	
+};
