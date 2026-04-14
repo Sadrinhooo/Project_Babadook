44d652d5c43f0308428dbf51d710ffcf6c9a9107
Sadrinhooo <sadrabazi82@gmail.com>
2026-04-14 18:08:08 +0200

    Flashlight added

---
+++ Source/Projekt_Babadook/FlashlightComponent.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FlashlightComponent.h"
+
+// Sets default values for this component's properties
+UFlashlightComponent::UFlashlightComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	
+	FlashlightMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	FlashlightMesh->SetupAttachment(this);
+	
+	LightComponent = CreateDefaultSubobject<USpotLightComponent>(TEXT("Spotlight"));
+	LightComponent->SetupAttachment(FlashlightMesh);
+
+	// ...
+}
+
+
+// Called when the game starts
+void UFlashlightComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UFlashlightComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/Projekt_Babadook/FlashlightComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SpotLightComponent.h"
+#include "FlashlightComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJEKT_BABADOOK_API UFlashlightComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UFlashlightComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, 
+		FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* FlashlightMesh;
+	
+	UPROPERTY(VisibleAnywhere)
+	USpotLightComponent* LightComponent;
+		
+};
