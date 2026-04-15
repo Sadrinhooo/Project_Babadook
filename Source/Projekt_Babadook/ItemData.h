64f741ae571ac5750f0f19ce8ad78ca8b409ac27
Sadrinhooo <sadrabazi82@gmail.com>
2026-04-15 17:37:09 +0200

    Item init

---
+++ Source/Projekt_Babadook/ItemData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJEKT_BABADOOK_API UItemData : public UObject
+{
+	GENERATED_BODY()
+	
+};
