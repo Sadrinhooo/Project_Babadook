c567a9469972b08f2c31cc3b4d324f962144af37
Sadrinhooo <sadrabazi82@gmail.com>
2026-04-29 11:37:23 +0200

    reverted shit

--- Source/Projekt_Babadook/Interactable.h
+++ Source/Projekt_Babadook/Interactable.h
@@ -25,8 +25,5 @@ public:
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 
 	virtual const FString& GetInteractPrompt(ACharacter* Interactor) = 0;
-	
-	UFUNCTION(BlueprintCallable)
-	virtual void PlaySFX();
 
 };
