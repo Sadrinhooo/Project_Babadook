da23bcad3020b74e751ea8d2a17d4276a60f1e3b
JohannesLagnefeldt <90642016+JohannesLagnefeldt@users.noreply.github.com>
2026-05-12 09:47:16 +0200

    Squashed commit of the following:
    commit 5b5dbce44de6844140cc76cd3d7046a032c2f636
    Merge: c88760c 3212419
    Author: Kai <kaisooksawat@protonmail.com>
    Date:   Tue May 12 09:31:04 2026 +0200
    
        Merge branch 'Kai2.0' into MergeTest_Giterrors
    
    commit 3212419e33c40d44805d4987310b059b55d4414b
    Author: Kai <kaisooksawat@protonmail.com>
    Date:   Tue May 12 09:30:44 2026 +0200
    
        Updated directional light
    
    commit c88760c905c85545a730ed9a5e9b14a0c03eadbd
    Merge: c1f9e6b 6a6db56
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Tue May 12 09:27:50 2026 +0200
    
        Merge branch 'MergeTest_Giterrors' into Sadra
    
    commit c1f9e6b3880ce8b07990e44bb35ff857b4ee2bcb
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Tue May 12 09:24:50 2026 +0200
    
        Signa
    
    commit 57b3b1599b8fd8a901d8205071ec194883240410
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Tue May 12 09:20:34 2026 +0200
    
        Shadow puzzle shit
    
    commit f56f86f5027bac82f978afd3288fe61df62298ed
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Tue May 12 08:37:32 2026 +0200
    
        Inför imorn
    
    commit 6a6db56ffc81322917210dd936846e0773f0c9a2
    Merge: a416bbd fe3a553
    Author: JohannesLagnefeldt <90642016+JohannesLagnefeldt@users.noreply.github.com>
    Date:   Tue May 12 08:26:46 2026 +0200
    
        Merge branch 'Yasna' into MergeTest_Giterrors
    
    commit cd22242bc0cdbabf5ebcbe686f7f3943f721eb55
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Mon May 11 21:24:20 2026 +0200
    
        MicroBSPush
    
    commit fe3a55382f527d54349a82d74a976750d6a221d7
    Author: Yaxna <yaxna2005@gmail.com>
    Date:   Mon May 11 20:46:31 2026 +0200
    
        SpritGamepadFix
    
    commit 473b6f3327d7214c63827e1930c26061a009ed22
    Author: Yaxna <yaxna2005@gmail.com>
    Date:   Mon May 11 20:44:32 2026 +0200
    
        MenuGamepad
    
    commit 045263974e98fdd876d0dcc45e0b6cf90b763749
    Author: Yaxna <yaxna2005@gmail.com>
    Date:   Mon May 11 19:38:46 2026 +0200
    
        Små fix
    
    commit 8e2bd2105e00014923cc4ac206df07260c864980
    Author: Yaxna <yaxna2005@gmail.com>
    Date:   Mon May 11 19:37:22 2026 +0200
    
        Fade Animation
    
    commit 54b92541c6b613c2837a47b842b3abe22b55053d
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Mon May 11 18:42:30 2026 +0200
    
        Some bs
    
    commit 95a5f7c07d7e03a8a4b248582b7c9d4d5a82e962
    Author: Yaxna <yaxna2005@gmail.com>
    Date:   Mon May 11 18:05:50 2026 +0200
    
        Små fix
    
    commit 90c68d28ceb1c58a974417782c88053fe5c0a839
    Author: Sadrinhooo <sadrabazi82@gmail.com>
    Date:   Mon May 11 17:27:21 2026 +0200
    
        DefaultState Created

--- Source/Projekt_Babadook/ShadowPuzzleManager.cpp
+++ Source/Projekt_Babadook/ShadowPuzzleManager.cpp
@@ -1,7 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "MyGameMode.h"
 #include "ShadowPuzzleManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "KeyItem.h"
+#include "MyPlayerController.h"
+#include "ShadowPuzzleDefaultState.h"
+
 
 // Sets default values
 AShadowPuzzleManager::AShadowPuzzleManager()
@@ -15,22 +20,72 @@ AShadowPuzzleManager::AShadowPuzzleManager()
 void AShadowPuzzleManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	DefaultState = NewObject<UShadowPuzzleDefaultState>(this);
+	SolvingState = NewObject<UShadowPuzzleSolvingState>(this);
+	ChangeState(DefaultState);
 }
 
 // Called every frame
 void AShadowPuzzleManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if ((FVector::DotProduct(PuzzleItemPawn->GetActorForwardVector(), TargetForwardVector)) > 0.99f &&
+		FVector::DotProduct(PuzzleItemPawn->GetActorUpVector(), TargetUpVector) > 0.99f)
+	{
+		OnSuccess(DeltaTime);
+	}
 
 }
 
 void AShadowPuzzleManager::Interact(ACharacter* Interactor)
 {
+	PuzzleState->Interact(Interactor);
 }
 
 const FString& AShadowPuzzleManager::GetInteractPrompt(ACharacter* Interactor)
 {
-	return SIGMA;
+	return PuzzleState->GetInteractPrompt(Interactor);
+}
+
+void AShadowPuzzleManager::ChangeState(UObject* NewState)
+{
+	PuzzleState = NewState;
+	PuzzleState->InitiateState();
+}
+
+bool AShadowPuzzleManager::PlayerHasKeyItem(int32& OutIndex)
+{
+	for (int i = 0; i < Cast<AMyGameMode>(UGameplayStatics::GetGameMode(this))->SharedInventory.Num(); i++)
+	{
+		if (Cast<AMyGameMode>(UGameplayStatics::GetGameMode(this))->SharedInventory[i].ItemTag == RequiredKeyItemTag)
+		{
+			OutIndex = i;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void AShadowPuzzleManager::OnSuccess(float DeltaTime)
+{
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	APawn* PlayerPawn = PC->GetPawn();
+
+	PlayerPawn->DisableInput(PC);
+	FQuat TargetQuat = TargetRotation.Quaternion();
+	FQuat CurrentQuat = PuzzleItemPawn->GetActorQuat();
+	FQuat NewQuat = FQuat::Slerp(CurrentQuat, TargetQuat, DeltaTime * 0.05);
+	NewQuat.Normalize();
+	PuzzleItemPawn->SetActorRotation(NewQuat);
+	GetWorld()->GetTimerManager().SetTimer(WaitHandle, [this]()
+	{
+		//ALL BS
+		UE_LOG(LogTemp, Warning, TEXT("SIGMA SIGMA ON THE ALL"));
+		EnableInput(GetWorld()->GetFirstPlayerController());
+		Cast<AMyPlayerController>(GetWorld()->GetFirstPlayerController())->UnpossessPuzzlePawn();
+		PrimaryActorTick.bCanEverTick = false;
+    
+	}, 3.0f, false); 
 }
 
