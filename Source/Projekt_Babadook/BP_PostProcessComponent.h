28629c19bb2c14127d577373f634b5a97c74dd8e
Kai <kaisooksawat@protonmail.com>
2026-05-21 22:23:50 +0200

    Post process

---
+++ Source/Projekt_Babadook/BP_PostProcessComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PostProcessComponent.h"
+#include "BP_PostProcessComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJEKT_BABADOOK_API UBP_PostProcessComponent : public UPostProcessComponent
+{
+	GENERATED_BODY()
+	
+};
