c0b4817157cc4f039b93d1f66eac4bc867b88640
Sadrinhooo <sadrabazi82@gmail.com>
2026-05-26 09:20:44 +0200

    små fixes

--- Source/Projekt_Babadook/BasePlayerCharacter.h
+++ Source/Projekt_Babadook/BasePlayerCharacter.h
@@ -71,7 +71,7 @@ public:
 	void DecreaseLanternOil(float DeltaTime) ;
 
 	UFUNCTION(BlueprintImplementableEvent)
-	void ShowInteractPrompt(const FString& Prompt, const FVector InteractableLocation, const UTexture2D* Widget);
+	void ShowInteractPrompt(const FString& Prompt, const FVector InteractableLocation, const UTexture2D* IconTexture);
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void ClearInteractPrompt();
